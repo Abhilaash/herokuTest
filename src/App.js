7afb314c466c27ae4d93ba5ba03fcb3dd8d997c4
Abhilaash <abhilaash.velamati@gmail.com>
2020-04-14 18:11:46 -0400

    timer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Hooks from './Hooks';
 
 function App() {
   return (
+    {item ? (
+      <img style={{ width: 210, height: 118 }} alt={item.title} src={item.src} />
+    ) : (
+      <Skeleton variant="rect" width={210} height={118} />
+    )}
     <div className="App">
       <Hooks/>
     </div>
